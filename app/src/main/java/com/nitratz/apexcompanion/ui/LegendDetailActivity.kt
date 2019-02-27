40c22ac215d25715ae012a03e053adb78ce2f3d6
Maxime <maxime.liege@studi.fr>
2019-02-27 11:56:31 +0100

    Adding launcher icons & abilities icons
    * Added vectors
    * Added abilities icons
    * Added launcher icons

--- app/src/main/java/com/nitratz/apexcompanion/ui/LegendDetailActivity.kt
+++ app/src/main/java/com/nitratz/apexcompanion/ui/LegendDetailActivity.kt
@@ -1,31 +1,46 @@
 package com.nitratz.apexcompanion.ui
 
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
 import android.widget.TextView
 import com.nitratz.apexcompanion.R
 import com.nitratz.apexcompanion.models.Legend
+import android.text.method.ScrollingMovementMethod
+import android.view.MenuItem
+
+
+
 
 class LegendDetailActivity : AppCompatActivity() {
 
     private lateinit var mTvLegendTitle: TextView
     private lateinit var mIvLegendImage: ImageView
+    private lateinit var mTvLegendDesc: TextView
 
     private lateinit var mLegend: Legend
 
     companion object {
-        val LEGEND_OBJECT = "legend_object"
+        const val LEGEND_OBJECT = "legend_object"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.legend_detail_activity)
 
+        mLegend = intent.getSerializableExtra(LEGEND_OBJECT) as Legend
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = mLegend.mName
+
         mIvLegendImage = findViewById(R.id.legend_image)
         mTvLegendTitle = findViewById(R.id.tv_title_legend)
+        mTvLegendDesc = findViewById(R.id.tv_desc_legend)
 
-        mLegend = intent.getSerializableExtra(LEGEND_OBJECT) as Legend
+        mTvLegendDesc.movementMethod = ScrollingMovementMethod()
+
+        mTvLegendTitle.text = mLegend.mName
+        mTvLegendDesc.text = mLegend.mDescription
         setImageByLegendId()
     }
 
@@ -40,5 +55,12 @@ class LegendDetailActivity : AppCompatActivity() {
             6 -> mIvLegendImage.setImageResource(R.drawable.legends_mirage)
             7 -> mIvLegendImage.setImageResource(R.drawable.legends_caustic)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> ActivityCompat.finishAfterTransition(this)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
